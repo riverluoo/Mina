983bd0cb47681aa504ad3759ef2c8d9d3fdef489
wangyangyang <769789170@qq.com>
2018-07-01 18:30:57 +0800

    服务端

--- src/main/java/com/riverluoo/MinaServer.java
+++ src/main/java/com/riverluoo/MinaServer.java
@@ -1,9 +1,48 @@
 package com.riverluoo;
 
+
+import org.apache.mina.common.IdleStatus;
+import org.apache.mina.common.IoAcceptor;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.LineDelimiter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
 /**
  * @description:
  * @author: wangyang
  * @since: 下午5:14 2018/7/1
  */
 public class MinaServer {
+
+    static int PORT = 7081;
+
+    static IoAcceptor acceptor = null;
+
+    public static void main(String[] args) {
+        try {
+            acceptor = new NioSocketAcceptor();
+            // 编码过滤器
+            acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(
+                    new TextLineCodecFactory(Charset.forName("UTF-8"),
+                            LineDelimiter.MAC.getValue(),
+                            LineDelimiter.MAC.getValue())
+            ));
+
+            acceptor.getSessionConfig().setReadBufferSize(1024);
+            acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+            acceptor.setHandler(new MyHandler());
+
+            acceptor.bind(new InetSocketAddress(PORT));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("server : " + PORT);
+
+    }
+
 }
---
+++ src/main/java/com/riverluoo/MyHandler.java
@@ -0,0 +1,54 @@
+package com.riverluoo;
+
+import org.apache.mina.common.IdleStatus;
+import org.apache.mina.common.IoHandlerAdapter;
+import org.apache.mina.common.IoSession;
+
+import java.util.Date;
+
+/**
+ * @description:
+ * @author: wangyang
+ * @since: 下午5:44 2018/7/1
+ */
+public class MyHandler extends IoHandlerAdapter {
+
+
+    @Override
+    public void sessionCreated(IoSession session) throws Exception {
+        System.out.println(" myhandle : session : 创建");
+    }
+
+    @Override
+    public void sessionOpened(IoSession session) throws Exception {
+        System.out.println("myhandle : session : 打开");
+    }
+
+    @Override
+    public void sessionClosed(IoSession session) throws Exception {
+        System.out.println("myhandle : session  : 关闭  ");
+    }
+
+    @Override
+    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+        System.out.println("myhandle : session  : 空闲");
+    }
+
+    @Override
+    public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+        System.out.println("myhandle : 连接异常");
+    }
+
+    @Override
+    public void messageReceived(IoSession session, Object message) throws Exception {
+        String msg= message.toString();
+        Date date = new Date();
+        System.out.println("myhandle : 接收 ：" + msg);
+        session.write(date);
+    }
+
+    @Override
+    public void messageSent(IoSession session, Object message) throws Exception {
+        System.out.println("myhandle : session : 发送");
+    }
+}
